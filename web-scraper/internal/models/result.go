17a3534d8797e648af32e6e4755e55ca2e18d236
mcghieb <mcghie.blake@gmail.com>
2026-01-29 00:08:20 -0700

    update: scraper to scrape race results section

--- web-scraper/internal/models/result.go
+++ web-scraper/internal/models/result.go
@@ -1,13 +1,44 @@
 package models
 
+import "fmt"
+
 type RaceResult struct {
-	ID            string // HeatNo
-	KartID        int    // kart number
-	DriverID      string // CustID
-	Position      int
-	Penalties     int
-	BestLaptime   float32
-	AvgLaptime    float32
-	NumLaps       int
-	GapFromLeader float32
+	ID             string // HeatNo //FIXME: this is missing
+	KartID         int    // kart number
+	DriverID       string // CustID
+	Position       int
+	Penalties      int // FIXME: also missing
+	BestLaptime    float64
+	AvgLaptime     float64
+	NumLaps        int
+	GapFromLeader  float64
+	ProskillRating int // FIXME: add to db schema
+	// FIXME: missing: timestamp, track name
+}
+
+func (rr RaceResult) String() string {
+	return fmt.Sprintf(
+		"RaceResult{\n"+
+			"\tID: %s\n"+
+			"\tKartID: %d\n"+
+			"\tDriverID: %s\n"+
+			"\tPosition: %d\n"+
+			"\tPenalties: %d\n"+
+			"\tBestLaptime: %.3f\n"+
+			"\tAvgLaptime: %.3f\n"+
+			"\tNumLaps: %d\n"+
+			"\tGapFromLeader: %.3f\n"+
+			"\tProskillRating: %d\n"+
+			"}",
+		rr.ID,
+		rr.KartID,
+		rr.DriverID,
+		rr.Position,
+		rr.Penalties,
+		rr.BestLaptime,
+		rr.AvgLaptime,
+		rr.NumLaps,
+		rr.GapFromLeader,
+		rr.ProskillRating,
+	)
 }
--- web-scraper/internal/scraper/handlers.go
+++ web-scraper/internal/scraper/handlers.go
@@ -9,7 +9,9 @@ import (
 	"github.com/mcghieb/kart-stats/web-scraper/internal/scraper/cache"
 )
 
-// AttachHandlers attaches the HEAT SCRAPING HANDLERS. Name will change if a different handler set is needed for other scraping.
+// AttachHandlers attaches the HEAT SCRAPING HANDLERS.
+// Name will change if a different handler set is needed
+// for other scraping.
 func AttachHandlers(c *colly.Collector, cache *cache.Cache) {
 	c.OnHTML("a[href^='RacerHistory']", func(e *colly.HTMLElement) {
 		handleCreateUser(e, cache.Driver)
@@ -19,6 +21,7 @@ func AttachHandlers(c *colly.Collector, cache *cache.Cache) {
 		handleResultsTable(e, cache)
 	})
 
+	// FIXME:
 	c.OnHTML("table.LapTimes", handleTimeTable)
 }
 
@@ -37,16 +40,20 @@ func handleCreateUser(e *colly.HTMLElement, c *cache.Driver) {
 	}
 }
 
-// tbody
 func handleResultsTable(e *colly.HTMLElement, c *cache.Cache) {
+	// FIXME: find a way to add heatnumber to cached result
+
 	e.ForEach("tr[class^='Top3Winners']", func(i int, e *colly.HTMLElement) {
 		top3Row(i, e, c)
+	})
+
+	e.ForEach("tr[class^='RegularRow']", func(i int, e *colly.HTMLElement) {
+		regularRow(i, e, c)
 	})
 }
 
 func top3Row(i int, e *colly.HTMLElement, c *cache.Cache) {
-	idx := i + 1 // Determines which row parsing strategy to use
-	fmt.Printf("i: %d\n", idx)
+	idx := (i % 3) + 1 // Determines which row parsing strategy to use
 	switch idx {
 	case 1:
 		top3RowOne(e, c)
@@ -58,19 +65,93 @@ func top3Row(i int, e *colly.HTMLElement, c *cache.Cache) {
 		// TODO: ERROR
 	}
 
-	if idx%3 == 0 {
-		// update DB with cache.Race
+	if idx == 3 {
+		// TODO: update DB with cache.Race
+
+		fmt.Println(c.Race.Get(getHeatNum(e)))
 		// clear cache.Race
+		c.Race = cache.NewRaceCache()
 	}
 }
 
 func top3RowOne(e *colly.HTMLElement, c *cache.Cache) {
+	// set position
+	p := e.ChildText("td.Position")
+	position := 0
+	switch p {
+	case "Heat Winner:":
+		position = 1
+	case "2nd Place:":
+		position = 2
+	case "3rd Place:":
+		position = 3
+	}
+	updateResultInCache(e, c, func(r *models.RaceResult) {
+		r.Position = position
+	})
+
+	if err := setDriverID(e, c); err != nil {
+		// TODO: error handling
+	}
 }
 
 func top3RowTwo(e *colly.HTMLElement, c *cache.Cache) {
+	if err := setBestLaptime(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setNumLaps(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setLeaderGap(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setAvgLaptime(e, c); err != nil {
+		// TODO: error handling
+	}
 }
 
 func top3RowThree(e *colly.HTMLElement, c *cache.Cache) {
+	if err := setProskill(e, c); err != nil {
+		// TODO: error
+	}
+}
+
+func regularRow(i int, e *colly.HTMLElement, c *cache.Cache) {
+	if err := setDriverID(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setPosition(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setBestLaptime(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setNumLaps(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setLeaderGap(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setAvgLaptime(e, c); err != nil {
+		// TODO: error handling
+	}
+
+	if err := setProskill(e, c); err != nil {
+		// TODO: error
+	}
+
+	// Print completed race result
+	fmt.Println(c.Race.Get(getHeatNum(e)))
+
+	// FIXME: persist to db and clear cache.Race
 }
 
 // FIXME: Time Table Handler
